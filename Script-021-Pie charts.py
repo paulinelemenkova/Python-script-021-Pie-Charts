0f5b12b2dc5fbeff5afe3f0367f289510c9ec527
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-20 10:26:32 +0800

    Add files via upload

--- Script-021-Pie charts.py
+++ Script-021-Pie charts.py
@@ -1,52 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[82]:
-
-
-# library
+import os
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-import os
+
 os.chdir('/Users/pauline/Documents/Python')
 dfM = pd.read_csv("Tab-Morph.csv")
- 
+
 # dataset
-df = pd.DataFrame({'Pacific Plate':dfM.plate_pacif, 
-                   'Philippine Plate':dfM.plate_phill,
-                   'Mariana Plate':dfM.plate_maria, 
-                   'Caroline Plate':dfM.plate_carol},
+df = pd.DataFrame({'Pacific Plate':dfM.plate_pacif,
+                  'Philippine Plate':dfM.plate_phill,
+                  'Mariana Plate':dfM.plate_maria,
+                  'Caroline Plate':dfM.plate_carol},
                   index=dfM.profile)
 
-#ax = plt.subplot(111) 
-#wedges, texts = ax.pie(np.abs(np.random.randn(5)))
-#for w in wedges:
- #   w.set_linewidth(3)
- #   w.set_edgecolor('r')
-
-#plt.rcParams['patch.linewidth'] = 3 
-#plt.rcParams['patch.edgecolor'] = 'white'
-
 # plot chart
-df.plot(kind='pie', subplots=True, figsize=(10, 10),  legend=False, table=False, 
-        fontsize=8, 
-        sort_columns=True,
-        layout=(2, 2), colormap='tab20b', 
-        title='Mariana Trench: Pie Charts for the \nBathymetry distribution by tectonic plates', 
-       )
+df.plot(kind='pie', subplots=True, figsize=(10, 10),
+        legend=False, table=False,
+        fontsize=8, sort_columns=True,
+        layout=(2, 2), colormap='tab20b',
+        title='Mariana Trench: Pie charts for the \nsample points distribution by tectonic plates',
+        )
 
+# visualize and save
+plt.tight_layout()
+plt.savefig('plot_Pie.png', dpi=300)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
